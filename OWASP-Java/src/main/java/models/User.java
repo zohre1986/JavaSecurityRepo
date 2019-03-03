ad7badf7273a563b87ab3847a80e8787fab0e9b9
zohre1986 <mehran.jodavi@gmail.com>
2019-03-03 16:40:09 +0330

    Default Changelist

--- OWASP-Java/src/main/java/models/User.java
+++ OWASP-Java/src/main/java/models/User.java
@@ -14,11 +14,6 @@ public final class User implements Serializable {
     public final String username, role, password;
     public final Timestamp created_at, updated_at;
 
-    public User(String username, String role, String password, Timestamp created_at, Timestamp updated_at) {
-        this.username = username;
-        this.role = role;
-        this.password = password;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-    }
+
+
 }
