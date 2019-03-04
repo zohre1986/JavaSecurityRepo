a14f4abcb6ee71974d8a344addeed688754781ea
hourieh-jamali <hj.dinan2006@gmail.com>
2019-03-04 10:27:51 +0330

    Update AdminServlet.java

--- OWASP-Java/src/main/java/servlets/AdminServlet.java
+++ OWASP-Java/src/main/java/servlets/AdminServlet.java
@@ -108,6 +108,7 @@ public class AdminServlet extends HttpServlet {
             // return value not logged
             //FIXME: OWASP A1:2017 - Injection
             //FIXME: OWASP A8:2013 - CSRF
+//            st.executeUpdate(query.toString());
             int result = preparedStatement.executeUpdate();
             logger.info(result + " row(s) affected by update query.");
 
